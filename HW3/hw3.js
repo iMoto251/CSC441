a641cc8a231e99afe70d49fc5fa4de28e09a2157
iMoto251 <dbrider251@gmail.com>
2021-02-15 19:48:33 -0600

    a

--- HW3/hw3.js
+++ HW3/hw3.js
@@ -6,29 +6,42 @@ const fs = require('fs');
 const urlcc = 'https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-continent.json';
 const urlca = 'https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-abbreviation.json';
 const urlcapc = 'https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-capital-city.json';
-const cc = fs.createWriteStream(`${__dirname}/countrycontinent.json`);
-const ca = fs.createWriteStream(`${__dirname}/countryabbreviation.json`);
-const capc = fs.createWriteStream(`${__dirname}/countrycapital.json`);
+const ccfs = fs.createWriteStream(`${__dirname}/countrycontinent.json`);
+const cafs = fs.createWriteStream(`${__dirname}/countryabbreviation.json`);
+const capcfs = fs.createWriteStream(`${__dirname}/countrycapital.json`);
+
+const ccrs = fs.createReadStream(`${__dirname}/countrycontinent.json`);
+const cars = fs.createReadStream(`${__dirname}/countryabbreviation.json`);
+const capcrs = fs.createReadStream(`${__dirname}/countrycapital.json`);
 
 
-const main = async () => {
+const readIn = async () => {
     const responsecc = await fetch(urlcc);
-    responsecc.body.pipe(cc);
-    cc.on('close', () =>{
+    responsecc.body.pipe(ccfs);
+    ccfs.on('close', () =>{
         console.log('Continent.json is closed');
     });
     
     const responseca = await fetch(urlca);
-    responseca.body.pipe(ca);
-    ca.on('close', () =>{
+    responseca.body.pipe(cafs);
+    cafs.on('close', () =>{
         console.log('Abbreviation.json is closed');
     });
 
     const responsecapc = await fetch(urlcapc);
-    responsecapc.body.pipe(capc);
-    capc.on('close', () =>{
+    responsecapc.body.pipe(capcfs);
+    capcfs.on('close', () =>{
         console.log('Capital.json is closed');
     });
 }
 
-main();
+
+
+const writeTo = async () => {
+    await console.log(ccrs);
+    await console.log(cars);
+    await console.log(capcrs);
+
+}
+
+readIn();
